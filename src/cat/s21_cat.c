204b2ae9bbb25f7556226b11240e595f18353518
Rhydon Harrag <rhydonha@si-b1.kzn.21-school.ru>
2023-10-31 23:04:45 +0300

    initial commit

--- src/cat/s21_cat.c
+++ src/cat/s21_cat.c
@@ -1,23 +1,35 @@
 #include <stdio.h>
+#include <string.h>
 
 void print_file(char *name);
 
 int main(int argc, char **argv) {
+  int err = 0;
+  if (*argv[1] != '-' || strcmp(argv[1], "--") == 0) {
+    err = 1;
+  } else
+    printf("Correct");
+  if (err == 1) {
+    printf("Incorrect");
+    return 1;
+  } else
     for (int i = 1; i < argc; i++) {
-        print_file(argv[i]);
+      print_file(argv[i]);
     }
-    return 0;
+  return 0;
 }
+
+// void parse_flags(int argc, char **argv) {}
 
 void print_file(char *name) {
   FILE *f = fopen(name, "rt");
-    
+
   if (f != NULL) {
-  int c = fgetc(f);
-  while (c != EOF) {
-    putc(c, stdout);
-    c = fgetc(f);
-  }
-  fclose(f);
+    int c = fgetc(f);
+    while (c != EOF) {
+      putc(c, stdout);
+      c = fgetc(f);
+    }
+    fclose(f);
   }
 }
---
+++ src/cat/s21_cat.h
@@ -0,0 +1,8 @@
+#ifndef S21CAT_H
+#define S21CAT_H
+#include <stdio.h>
+
+
+void parse_flags(int argc, char **argv);
+
+#endif
