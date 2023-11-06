06571f6cbead35768ae568c2ac63e437cf46c06b
Александр Дубровин <xah799@mail.ru>
2023-11-06 12:57:51 +0300

    implemented check_argc() method and checked method

--- src/cat/s21_cat.c
+++ src/cat/s21_cat.c
@@ -1,38 +1,22 @@
 #include <stdio.h>
 #include <string.h>
-
-void print_file(char *name);
+#include "functions.h"
+#include "flags.h"
 
 int main(int argc, char **argv) {
   int err = 0;
-
-  if (*argv[1] != '-' || strcmp(argv[1], "--") == 0) {
-    err = 1;
-  } else
-    printf("Correct");
-  if (err == 1) {
-    printf("Incorrect");
-    return 1;
-  } else
-    for (int i = 1; i < argc; i++) {
-      print_file(argv[i]);
-    }
-  return 0;
+  if (check_argc(argc) == 1)
+  {
+    printf("Error");
+  } else {
+    printf("OK");
+  }
+  return err;
 }
 
-//int check
-
-// void parse_flags(int argc, char **argv) {}
-
-void print_file(char *name) {
-  FILE *f = fopen(name, "rt");
-
-  if (f != NULL) {
-    int c = fgetc(f);
-    while (c != EOF) {
-      putc(c, stdout);
-      c = fgetc(f);
-    }
-    fclose(f);
-  }
+int check_argc(int argc) {
+  int err = 0;
+  if (argc < 2)  
+    err = 1;
+  return err;
 }
