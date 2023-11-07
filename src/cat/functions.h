43c48f4949b4176b106c67597dd7278ec972bd12
Rhydon Harrag <rhydonha@ev-h5.kzn.21-school.ru>
2023-11-07 09:04:53 +0300

    added read_flags() and init_flags() methods and change start_cat() method parameters

--- src/cat/functions.h
+++ src/cat/functions.h
@@ -1,9 +1,16 @@
 #ifndef FUNCTIONS_H
 #define FUNCTIONS_H
 #include <stdio.h>
+#include "flags.h"
 
 // Метод для старта программы
-int start_cat(int argc);
+int start_cat(int argc, char **argv);
+
+// Метод чтения флагов
+int read_flags(int argc, char **argv, flags *flags);
+
+// Метод инициализации флагов
+int init_flags(int flag, flags *flags);
 
 // Проверка на количество аргументов
 int check_argc(int argc);
