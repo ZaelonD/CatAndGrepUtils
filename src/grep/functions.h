d4d91c51df7d650f50f6253080be59209e8f9410
Rhydon Harrag <rhydonha@ev-f5.kzn.21-school.ru>
2023-11-18 15:45:43 +0300

    announced start_grep() method in function.h

--- src/grep/functions.h
+++ src/grep/functions.h
@@ -4,7 +4,13 @@
 
 #include "flags.h"
 
+// Метод для старта программы
+int start_grep(int argc, char **argv);
+
+// Метод для чтения флагов
 int read_flags(int argc, char **argv, flags *flags);
+
+// Метод для инициализации флагов
 int init_flags(int flag, flags *flags);
 
 #endif
