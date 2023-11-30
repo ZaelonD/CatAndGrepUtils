21d88f7b7d8cb32c59fda1cbe306a0cee2a1a93d
Rhydon Harrag <zaelon@MY-LAPTOP>
2023-11-30 17:00:38 +0300

    created flags_functions files for flags functions method, implemented methods for -v and -n flags

---
+++ src/grep/flags_functions.h
@@ -0,0 +1,10 @@
+#ifndef FLAGS_FUNCTIONS
+#define FLAGS_FUNCTIONS
+#include <stdio.h>
+
+void apply_n_flag(int contains, int files_count, char *file_name, char *string,
+                  int string_count);
+
+void apply_v_flag(int contains, int files_count, char *file_name, char *string);
+
+#endif
