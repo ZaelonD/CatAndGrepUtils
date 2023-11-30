31f61ef9d68df393ac06f4035e5b255f3469f3ed
Rhydon Harrag <rhydonha@pr-c6.kzn.21-school.ru>
2023-11-30 18:38:21 +0300

    added comments to methods

--- src/grep/flags_functions.h
+++ src/grep/flags_functions.h
@@ -4,15 +4,19 @@
 
 #include "flags.h"
 
+// Метод для использования флага -n
 void apply_n_flag(int contains, int files_count, char *file_name, char *string,
                   int string_count, flags *flags);
 
+// Метод для использования флага -v
 void apply_v_flag(int contains, int files_count, char *file_name, char *string,
                   flags *flags);
 
+// Метод для использования флага -e
 void apply_e_flag(int contains, int files_count, char *file_name, char *string,
                   flags *flags);
 
+// Метод для использования флага -i
 void apply_i_flag(int contains, int files_count, char *file_name, char *string,
                   flags *flags);
 
